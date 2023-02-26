42a9306a0fcf63c84989b4284378809f030d3483
kota65535 <kota65535@gmail.com>
2023-02-27 02:10:24 +0900

    b

--- src/main.js
+++ src/main.js
@@ -1,24 +1,20 @@
 const exec = require("./exec");
 
 function main(inputs) {
+  // Set configs from dynamic inputs
   for (const s of inputs.scopes) {
     for (const [k, v] of Object.entries(inputs.configs)) {
       exec("git", ["config", `--${s}`, k, v]);
     }
   }
-  configureGitHubToken(inputs.githubToken);
-}
 
-function configureGitHubToken(inputs) {
-  if (!inputs.githubToken) {
-    return;
-  }
-
-  const base64Token = Buffer.from(`${inputs.githubToken}:`).toString("base64");
-
-  for (const s of inputs.scopes) {
-    exec("git", ["config", `--${s}`, "http.https://github.com/.extraheader", `Authorization: Basic ${base64Token}`]);
-    exec("git", ["config", `--${s}`, "url.https://github.com/.insteadOf", "git@github.com:"]);
+  if (inputs.githubToken) {
+    // Configure credentials if github-token input presents
+    const base64Token = Buffer.from(`${inputs.githubToken}:`).toString("base64");
+    for (const s of inputs.scopes) {
+      exec("git", ["config", `--${s}`, "http.https://github.com/.extraheader", `Authorization: Basic ${base64Token}`]);
+      exec("git", ["config", `--${s}`, "url.https://github.com/.insteadOf", "git@github.com:"]);
+    }
   }
 }
 
