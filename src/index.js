d3d703ea7ae8610735b66825acc62ae6149268ce
Tomohiko Ozawa <kota65535@gmail.com>
2025-03-20 00:15:21 +0900

    Add cleanup (#28)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 const core = require("@actions/core");
 const main = require("./main");
 const { getInputs } = require("./input");
+const post = require("./post");
 
-try {
-  const inputs = getInputs();
-  main(inputs);
-} catch (error) {
-  core.setFailed(error.message);
+const isPost = core.getState("isPost");
+const inputs = getInputs();
+
+if (isPost) {
+  // cleanup
+  try {
+    post(inputs);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+} else {
+  // main
+  try {
+    main(inputs);
+  } catch (error) {
+    core.setFailed(error.message);
+  } finally {
+    // cf. https://github.com/actions/checkout/blob/main/src/state-helper.ts
+    core.saveState("isPost", "true");
+  }
 }
